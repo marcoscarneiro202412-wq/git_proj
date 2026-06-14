00cde4b122fa96637f6904ad085b4b93310aa4f5
marcoscarneiro202412-wq <marcoscarneiro202412@gmail.com>
2026-06-14 17:08:10 -0300

    botando um número que funcione kkk

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const { default: baskara } = require("./baskara.ts");
 const { default: calcular } = require("./calcular.ts");
 
 console.log(baskara(4, undefined, -16));
-console.log(calcular(1, baskara(4, undefined, -16)))
+console.log(calcular(1, baskara(4, 20, 16)))
