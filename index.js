435243b9a3842c4c59a15e9854215264b4f54174
marcoscarneiro202412-wq <marcoscarneiro202412@gmail.com>
2026-06-14 17:06:45 -0300

    Colocando que calcule a soma das soluções reais

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { default: baskara } = require("./baskara.ts");
 const { default: calcular } = require("./calcular.ts");
 
-console.log(baskara(4, undefined, -16))
+console.log(baskara(4, undefined, -16));
+console.log(calcular(1, baskara(4, undefined, -16)))
