2b2b042d9ce33d890b582a48adef4f4621b3a245
marcoscarneiro202412-wq <marcoscarneiro202412@gmail.com>
2026-06-16 18:19:57 -0300

    Creating tan

--- trigonometry.ts
+++ trigonometry.ts
@@ -2,3 +2,5 @@ const sin = (val: number) =>  Math.sin(val)*(Math.PI/180);
 
 
 const cos = (val: number) =>  Math.cos(val)*(Math.PI/180);
+
+const tan = (val: number) =>  Math.tan(val)*(Math.PI/180);
