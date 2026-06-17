7e95ed138f726d764c6432fd661141f5d891be5a
marcoscarneiro202412-wq <marcoscarneiro202412@gmail.com>
2026-06-17 20:55:26 -0300

    Asking trigonometry

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const { default: baskara } = require("./baskara.ts");
 const { default: calcular } = require("./calcular.ts");
-const { cos, sin, tan } = require("./trigonometry.ts");
-const {triSide} = require("./tri-sides.ts")
 
 const readline = require("readline").createInterface({
   input: process.stdin,
@@ -24,38 +22,6 @@ readline.question("Qual é a operação calcular ou bhaskara? ", (op) => {
           readline.close();
         });
       });
-    });
-  }
-  if (op == 2) {
-    readline.question("Sen, cos, tg? ", (n) => {
-      const opt = +n;
-      readline.question("Valor? ", (n) => {
-        const v = +n;
-        if (v === 0 || !opt) return;
-        switch (opt) {
-          case 0:
-            console.log(sin(v));
-            break;
-          case 1:
-            console.log(cos(v));
-            break;
-          case 2:
-            console.log(tan(v));
-            break;
-          default:
-            console.log("Ai é d+ po");
-            break;
-            8;
-        }
-        readline.close();
-      });
-    });
-  }
-  if (op == 3) {
-    readline.question("Quais os lados a, b, c? (Separe por espaços) ", (r) => {
-      const sides = r.split(" ");
-      console.log(triSide({ a: sides[0], b: sides[1], c: sides[2] }));
-      readline.close()
     });
   }
 });
