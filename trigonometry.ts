ec594d60fa40c5799cbbe8db3da0c073423a86a0
marcoscarneiro202412-wq <marcoscarneiro202412@gmail.com>
2026-06-17 20:55:26 -0300

    Asking trigonometry

--- trigonometry.ts
+++ trigonometry.ts
@@ -1,6 +1,6 @@
-const sin = (val: number) =>  Math.sin(val)*(Math.PI/180);
+export const sin = (val: number) =>  Math.sin(val)*(Math.PI/180);
 
 
-const cos = (val: number) =>  Math.cos(val)*(Math.PI/180);
+export const cos = (val: number) =>  Math.cos(val)*(Math.PI/180);
 
-const tan = (val: number) =>  Math.tan(val)*(Math.PI/180);
+export const tan = (val: number) =>  Math.tan(val)*(Math.PI/180);
