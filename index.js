9824965da375b4921189ad94e40cff03bf236324
marcoscarneiro202412-wq <marcoscarneiro202412@gmail.com>
2026-06-17 20:47:43 -0300

    Asking sum

--- index.js
+++ index.js
@@ -1,4 +1,43 @@
 const { default: baskara } = require("./baskara.ts");
 const { default: calcular } = require("./calcular.ts");
 
-console.log(baskara(4, undefined, -16))
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+readline.question("Qual é a operação calcular ou bhaskara? ", (op) => {
+  if (op == 0) {
+    readline.question("1 num? ", (n) => {
+      const num1 = +n;
+      readline.question("2 num? ", (n) => {
+        const num2 = +n;
+        if (Number.isNaN(num1) || Number.isNaN(num2)) {
+          console.log("Ai tu me quebra");
+          readline.close();
+        } else {
+          console.log(calcular(1, [num1, num2]));
+          readline.close();
+        }
+      });
+    });
+  }
+  if (op == 1) {
+    readline.question("Qual é a? ", (num) => {
+      const a = +num;
+      readline.question("Qual é b? ", (num) => {
+        const b = +num === 0 ? undefined : +num;
+        readline.question("Qual é c? ", (num) => {
+          const c = +num === 0 ? undefined : +num;
+          if (a === 0 || (b === undefined && c === undefined)) {
+            console.log("Poxa mn n sabe fzr n eh?");
+            readline.close();
+          } else {
+            readline.close();
+            console.log(baskara(a, b, c));
+          }
+        });
+      });
+    });
+  }
+});
