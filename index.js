5657e4dad6573727c50e7f751437c5f60937313a
marcoscarneiro202412-wq <marcoscarneiro202412@gmail.com>
2026-06-17 20:43:56 -0300

    Creating baskara ask

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 const { default: baskara } = require("./baskara.ts");
 const { default: calcular } = require("./calcular.ts");
 
-console.log(baskara(4, undefined, -16));
-console.log(calcular(1, baskara(4, 20, 16)))
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+readline.question("Qual é a operação calcular ou bhaskara? ", (op) => {
+  if (op == 1) {
+    readline.question("Qual é a? ", (num) => {
+      const a = +num;
+      readline.question("Qual é b? ", (num) => {
+        const b = +num === 0 ? undefined : +num;
+        readline.question("Qual é c? ", (num) => {
+          const c = +num === 0 ? undefined : +num;
+          if (a === 0 || (b === undefined && c === undefined)) {
+            console.log("Poxa mn n sabe fzr n eh?");
+          } else {
+            console.log(baskara(a, b, c));
+          }
+          readline.close();
+        });
+      });
+    });
+  }
+});
